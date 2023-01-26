1f78c2ec7f4fadd45ad03bfec840267b613bf16f
AntimonyIQ <antimonyiq@gmail.com>
2023-01-26 20:35:17 +0100

    Added index file

---
+++ app.js
@@ -0,0 +1,18 @@
+var study = new Vue({
+  el: "#vue-app",
+  data: {
+    heading: "Qubit data & Quantum Infrastructure Provider",
+    text: "Happiness makes up in height what it lacks in length.",
+    appTitle: "Stateless Technology",
+    headingParagraph: "The third generation of the WWW is quantum. Build fast, cutting-edge quantum apps using our qubit technology infrastructure.",
+    singleState: "a|0》+ b|1》",
+    CardTitle: "Dovona",
+    CardText: "Get started learning Dovona hybrid programming language.",
+    WhoWeAre: "Welcome to Stateless Technology",
+    WhatWeDo: "We are a quantum computing company that specializes in providing advanced quantum computing infrastructure and data services to organizations and researchers. We utilizes the latest technologies to design, build, and maintain powerful quantum systems that can handle even the most complex data processing tasks. ",
+    LearnTitle: "Open guide and public resources.",
+    IndustriesTitle: "Industrial technologies on quantum Infrastructure", 
+    CareerTitle: "Careers at Stateless Technology",
+    SolutionsTitle: "Stateless Technology Solutions"
+  }
+});
